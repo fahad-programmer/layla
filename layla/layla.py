5d009e3f83f62bf2e9141d284c7b472b3d7dba42
Imran Akbar <infotainer04@gmail.com>
2021-01-08 14:36:05 +0500

    Joined the work and layla main files

--- layla/layla.py
+++ layla/layla.py
@@ -6,6 +6,8 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 
+from work.work import layla_com, wish_me, take_command, usrname
+
 from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
@@ -17,10 +19,12 @@ model = load_model('layla_model.h5')
 
 print(classes)
 
+
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
+
 
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
@@ -30,6 +34,7 @@ def bag_of_words(sentence):
             if word == w:
                 bag[i] = 1
     return np.array(bag)
+
 
 def predict_class(sentence):
     bow = bag_of_words(sentence)
@@ -43,6 +48,7 @@ def predict_class(sentence):
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
 
+
 def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
@@ -50,17 +56,26 @@ def get_response(intents_list, intents_json):
         if i['tag'] == tag:
             result = random.choice(i['responses'])
             return result
-            break
 
-print("GO! layla is listening")
 
 """
 This is a basic code...
 everything will be working inside the work folder
 we will just import work.py function in this while loop
 """
+clear = lambda: os.system('cls')
+
+# This Function will clean any
+# command before execution of this python file
+
+wish_me()
+usrname()
+
 while True:
-    message = input(">> ")
-    ints = predict_class(message)
+    query = take_command().lower()
+    ints = predict_class(query)
     res = get_response(ints, intents)
-    print(res)
+    if res == True:
+        print(res)
+    else:
+        work.layla_com(query)
