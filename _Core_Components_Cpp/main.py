49905cb8f11225c4cc137e93f1dd0f8700a23c25
fahadmalik-creator <unexpectedprogrammer@gmail.com>
2021-01-14 19:29:41 +0500

    Made Some Changes To The Previos Files And Changed Some Function From C To C++ Due to Performance Issue And Also Added New Functions

--- _Core_Components_Cpp/main.py
+++ _Core_Components_Cpp/main.py
@@ -1,12 +1,26 @@
 import random
 import datetime
+import webbrowser
+
 
 def greet():
-	responses = ["Hi, Sir", "Hello Sir, Its Good To Hear You", "How's Doing Man", "Hello Handsome"]
-	response = random.choice(responses)
-	return response
+    responses = [
+        "Hi, Sir", "Hello Sir, Its Good To Hear You", "How's Doing Man",
+        "Hello Handsome"
+    ]
+    response = random.choice(responses)
+    return response
+
 
 def current_date():
-	date = datetime.datetime.now()
-	main_date = f"{date.strftime('%d')},{date.strftime('%B')}{date.strftime('%Y')}"
-	return main_date
+    date = datetime.datetime.now()
+    main_date = f"{date.strftime('%d')},{date.strftime('%B')}{date.strftime('%Y')}"
+    return main_date
+
+
+def addition(query):
+    string_numbers = [int(s) for s in query.split() if s.isdigit()]
+    add_numbers = sum(string_numbers)
+    return_answer = f"The Result Of The Addition Is {add_numbers}".encode(
+        'UTF-8')
+    return return_answer
