fd81f7954e6d8699b7c2b72ba78b8c4ab9e2de66
Imran Akbar <infotainer04@gmail.com>
2021-01-29 15:15:45 +0500

    Added a main logic to run all the google scraps at a time

--- Web Scraping/googledata.py
+++ Web Scraping/googledata.py
@@ -20,39 +20,70 @@ class google_results:
         # The code to get the html contents here.
 
         soup = BeautifulSoup(html, 'lxml')
-        # self.scrap_capital(soup)
+        self.scrap_capital(soup)
+        print("\n\nRelated Queries: ")
         self.related_queries(soup)
     
     def scrap_capital(self, soup) -> str:
 
         # Find all the search result divs
-        # divs = soup.select("div.Z0LcW")       # One Word Answer > a
-        # divs = soup.select("div.kno-rdesc")     # Wikipedia answer
-        # divs = soup.select("div.LGOjhe")        # Info Box Answer > span
-        # divs = soup.select("div.c4bQHf")        # population
-        # divs = soup.select("div.sL6Rbf")      # Local Time
-        # divs = soup.select("div.dAassd")      # Founders
-        # divs = soup.select("div.LwV4sf")      # Founder's 2
-        # divs = soup.select("div.HwtpBd")      # Answer in one word and below is explainantion
-        # divs = soup.select("div.di3YZe")        # List data > divto get the heading, li to get the list
-        # divs = soup.select("div.OTnkWc")  # Quotes
-        # divs = soup.select("div.lMmzdb")      # Distance between two places > div
-        # divs = soup.select("div.vk_c")      # Distance between two countries or two cities in diffrent countries > div
-        # divs = soup.select("div.dAassd")    # Movies List
+        try:
+            divs = soup.select("div.Z0LcW") # One Word Answer > a
+            ans = self.for_loop(divs)
+            if len(ans) >=1:
+                print(ans)
+                return
+            else:
+                pass
+        except:
+            pass
+        try:
+            divs = soup.select("div.HwtpBd") # Answer in one word and below is explainantion > a
+            ans = self.for_loop(divs)
+            if len(ans) >=1:
+                print(ans)
+                return
+            else:
+                pass
+        except:
+            pass
+        try:
+            divs = soup.select("div.LGOjhe") # Info Box Answer > span
+            ans = self.for_loop(divs)
+            if len(ans) >=1:
+                print(ans)
+                return
+            else:
+                pass
+        except:
+            pass
+        # 9# divs = soup.select("div.kno-rdesc")     # Wikipedia answer
+        # 4# divs = soup.select("div.c4bQHf")        # population
+        # 5# divs = soup.select("div.sL6Rbf")      # Local Time
+        # 0# divs = soup.select("div.dAassd")      # Founders
+        # 11# divs = soup.select("div.LwV4sf")      # Founder's 2
+        # 6# divs = soup.select("div.di3YZe")        # List data > divto get the heading, li to get the list
+        # 13# divs = soup.select("div.OTnkWc")  # Quotes
+        # 7# divs = soup.select("div.lMmzdb")      # Distance between two places > div
+        # 8# divs = soup.select("div.vk_c")      # Distance between two countries or two cities in diffrent countries > div
+        # 12# divs = soup.select("div.dAassd")    # Movies List
+    
+    def for_loop(self, divs):
         for div in divs:
             # Search for a h3 tag
-            # results = div.select("a")
+            results = div.select("a")
             # results = div.select("span")
             # results = div.select("li")
-            results = div.select("i")
-            # results = div.select("div")
+            # results = div.select("i")
+            
+            results = div.select("div")
 
             # Check if we have found a result
             if (len(results) >= 1):
 
                 # Print the title
                 h3 = results[0]
-                print(h3.get_text())
+                return(h3.get_text())
     
     def related_queries(self, soup) -> str:
         divs = soup.select("div.related-question-pair")
@@ -68,3 +99,4 @@ class google_results:
             
 # scrap_capital("what is the capital of russia")
 google_results("who was elsa")
+# google_results("american president")
