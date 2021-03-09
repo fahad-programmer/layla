6c6e35f675d0944dbe0230164df5700775e7f448
fahadmalik-creator <unexpectedprogrammer@gmail.com>
2021-03-10 00:22:35 +0500

    Successfully compiled the whole movie automation code to c language for performance using cython

--- automation/test.py
+++ automation/test.py
@@ -1,12 +1,3 @@
+from movies import download_movies
 
-from movies import get_trending_movies
-import timeit
-
-
-python_m = timeit.timeit("get_trending_movies()", number=1, setup="from movies_1 import get_trending_movies")
-c_m = timeit.timeit("get_trending_movies()", number=1, setup="from movies import get_trending_movies")
-
-print(c_m)
-print(python_m)
-
-print(f"C is {c_m/python_m}x times faster than python")
+download_movies('Rio')
