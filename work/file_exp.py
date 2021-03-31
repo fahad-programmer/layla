aea4809013dfda6a3a154ef33ffcd349911663d9
Imran Akbar <infotainer04@gmail.com>
2021-03-31 12:17:59 +0500

    Added Image encypter and decrypter

--- work/file_exp.py
+++ work/file_exp.py
@@ -181,6 +181,87 @@ class file_management:
                     zip.write(file)
   
             print('All files zipped successfully!')  
+            
+    def encrypt_image(self):
+        # try block to handle exception
+        try:
+            # take path of image as a input
+            path = input(r'Enter path of Image : ')
+            
+            # taking encryption key as input
+            key = int(input('Enter Key for encryption of Image : '))
+            
+            # print path of image file and encryption key that
+            # we are using
+            print('The path of file : ', path)
+            print('Key for encryption : ', key)
+            
+            # open file for reading purpose
+            fin = open(path, 'rb')
+            
+            # storing image data in variable "image"
+            image = fin.read()
+            fin.close()
+            
+            # converting image into byte array to 
+            # perform encryption easily on numeric data
+            image = bytearray(image)
+        
+            # performing XOR operation on each value of bytearray
+            for index, values in enumerate(image):
+                image[index] = values ^ key
+        
+            # opening file for writting purpose
+            fin = open(path, 'wb')
+            
+            # writing encrypted data in image
+            fin.write(image)
+            fin.close()
+            print('Encryption Done...')
+        
+            
+        except Exception:
+            print('Error caught : ', Exception.__name__)
+            
+    def decrypt_image(self):
+        # try block to handle the exception
+        try:
+            # take path of image as a input
+            path = input(r'Enter path of Image : ')
+            
+            # taking decryption key as input
+            key = int(input('Enter Key for encryption of Image : '))
+            
+            # print path of image file and decryption key that we are using
+            print('The path of file : ', path)
+            print('Note : Encryption key and Decryption key must be same.')
+            print('Key for Decryption : ', key)
+            
+            # open file for reading purpose
+            fin = open(path, 'rb')
+            
+            # storing image data in variable "image"
+            image = fin.read()
+            fin.close()
+            
+            # converting image into byte array to perform decryption easily on numeric data
+            image = bytearray(image)
+        
+            # performing XOR operation on each value of bytearray
+            for index, values in enumerate(image):
+                image[index] = values ^ key
+        
+            # opening file for writting purpose
+            fin = open(path, 'wb')
+            
+            # writing decryption data in image
+            fin.write(image)
+            fin.close()
+            print('Decryption Done...')
+        
+        
+        except Exception:
+            print('Error caught : ', Exception.__name__)
         
 # while True:
 #     try:
