f7d09c1f4d366265bd1885b8a91b0e8644513a86
Imran Akbar <infotainer04@gmail.com>
2021-03-31 11:38:53 +0500

    Added a system to create a new file with any type extension in directory we are currently in

--- work/file_exp.py
+++ work/file_exp.py
@@ -13,114 +13,147 @@ import subprocess
 import time
 import urllib.parse
 
-clsid = '{9BA05972-F6A8-11CF-A442-00A0C90A8F39}' #Valid for IE as well!
-
-def getEditText(hwnd):
-    # api returns 16 bit characters so buffer needs 1 more char for null and twice the num of chars
-    buf_size = (win32gui.SendMessage(hwnd, WM_GETTEXTLENGTH, 0, 0) +1 ) * 2
-    target_buff = ctypes.create_string_buffer(buf_size)
-    win32gui.SendMessage(hwnd, WM_GETTEXT, buf_size, ctypes.addressof(target_buff))
-    return target_buff.raw.decode('utf16')[:-1]# remove the null char on the end
-
-def _normaliseText(controlText):
-    '''Remove '&' characters, and lower case.
-    Useful for matching control text.'''
-    return controlText.lower().replace('&', '')
-
-def _windowEnumerationHandler(hwnd, resultList):
-    '''Pass to win32gui.EnumWindows() to generate list of window handle,
-    window text, window class tuples.'''
-    resultList.append((hwnd, win32gui.GetWindowText(hwnd), win32gui.GetClassName(hwnd)))
-
-def searchChildWindows(currentHwnd,
-               wantedText=None,
-               wantedClass=None,
-               selectionFunction=None):
-    results = []
-    childWindows = []
-    try:
-        win32gui.EnumChildWindows(currentHwnd,
-                      _windowEnumerationHandler,
-                      childWindows)
-    except win32gui.error:
-        # This seems to mean that the control *cannot* have child windows,
-        # i.e. not a container.
-        return
-    for childHwnd, windowText, windowClass in childWindows:
-        descendentMatchingHwnds = searchChildWindows(childHwnd)
-        if descendentMatchingHwnds:
-            results += descendentMatchingHwnds
-
-        if wantedText and \
-            not _normaliseText(wantedText) in _normaliseText(windowText):
-                continue
-        if wantedClass and \
-            not windowClass == wantedClass:
-                continue
-        if selectionFunction and \
-            not selectionFunction(childHwnd):
-                continue
-        results.append(childHwnd)
-    return results
-
-
-def explorer_fileselection():
-    global clsid
-    address_1=""
-    files = []
-    shellwindows = win32.Dispatch(clsid)
-    w=win32gui
-    window = w.GetForegroundWindow()
-    #print("window: %s" % window)
-    if (window != 0):
-        if (w.GetClassName(window) == 'CabinetWClass'): # the main explorer window
-            #print("class: %s" % w.GetClassName(window))
-            #print("text: %s " %w.GetWindowText(window))
-            children = list(set(searchChildWindows(window)))
-            addr_edit = None
-            file_view = None
-            for child in children:
-                if (w.GetClassName(child) == 'WorkerW'): # the address bar
-                    addr_children = list(set(searchChildWindows(child)))
-                    for addr_child in addr_children:
-                        if (w.GetClassName(addr_child) == 'ReBarWindow32'):
-                            addr_edit = addr_child
-                            addr_children = list(set(searchChildWindows(child)))
-                            for addr_child in addr_children:
-                                if (w.GetClassName(addr_child) == 'Address Band Root'):
-                                    addr_edit = addr_child
-                                    addr_children = list(set(searchChildWindows(child)))
-                                    for addr_child in addr_children:
-                                        if (w.GetClassName(addr_child) == 'msctls_progress32'):
-                                            addr_edit = addr_child
-                                            addr_children = list(set(searchChildWindows(child)))
-                                            for addr_child in addr_children:
-                                                if (w.GetClassName(addr_child) == 'Breadcrumb Parent'):
-                                                    addr_edit = addr_child
-                                                    addr_children = list(set(searchChildWindows(child)))
-                                                    for addr_child in addr_children:
-                                                        if (w.GetClassName(addr_child) == 'ToolbarWindow32'):
-                                                            text=getEditText(addr_child)
-                                                            if "\\" in text:
-                                                                address_1=getEditText(addr_child)[text.index(" ")+1:]
-                                                                print("Address --> "+address_1)
-
-    for window in range(shellwindows.Count):
-        window_URL = urllib.parse.unquote(shellwindows[window].LocationURL,encoding='ISO 8859-1')
-        window_dir = window_URL.split("///")[1].replace("/", "\\")
-        print("Directory --> "+window_dir)
-        if window_dir==address_1:
-            selected_files = shellwindows[window].Document.SelectedItems()
-            for file in range(selected_files.Count):
-                files.append(selected_files.Item(file).Path)
-            print("Files --> "+str(files))
+class file_management:
     
-    
-    
+    def __init__(self):
+        self.clsid = '{9BA05972-F6A8-11CF-A442-00A0C90A8F39}' #Valid for IE as well!
 
+    def getEditText(self, hwnd):
+        # api returns 16 bit characters so buffer needs 1 more char for null and twice the num of chars
+        self.buf_size = (win32gui.SendMessage(hwnd, WM_GETTEXTLENGTH, 0, 0) +1 ) * 2
+        self.target_buff = ctypes.create_string_buffer(self.buf_size)
+        win32gui.SendMessage(hwnd, WM_GETTEXT, self.buf_size, ctypes.addressof(self.target_buff))
+        return self.target_buff.raw.decode('utf16')[:-1]# remove the null char on the end
+
+    def _normaliseText(self, control_text):
+        '''Remove '&' characters, and lower case.
+        Useful for matching control text.'''
+        return control_text.lower().replace('&', '')
+
+    def _windowEnumerationHandler(self, hwnd, result_list):
+        '''Pass to win32gui.EnumWindows() to generate list of window handle,
+        window text, window class tuples.'''
+        result_list.append((hwnd, win32gui.GetWindowText(hwnd), win32gui.GetClassName(hwnd)))
+
+    def searchChildWindows(self, 
+                current_hwnd,
+                wanted_text=None,
+                wanted_class=None,
+                selection_function=None):
+        results = []
+        child_windows = []
+        try:
+            win32gui.EnumChildWindows(current_hwnd,
+                        self._windowEnumerationHandler,
+                        child_windows)
+        except win32gui.error:
+            # This seems to mean that the control *cannot* have child windows,
+            # i.e. not a container.
+            return
+        for child_hwnd, window_text, window_class in child_windows:
+            descendent_matching_hwnds = self.searchChildWindows(child_hwnd)
+            if descendent_matching_hwnds:
+                results += descendent_matching_hwnds
+
+            if wanted_text and \
+                not self._normaliseText(wanted_text) in self._normaliseText(window_text):
+                    continue
+            if wanted_class and \
+                not window_class == wanted_class:
+                    continue
+            if selection_function and \
+                not selection_function(child_hwnd):
+                    continue
+            results.append(child_hwnd)
+        return results
+
+
+    def explorer_fileselection(self):
+        global clsid
+        self.address_1=""
+        self.files = []
+        shellwindows = win32.Dispatch(self.clsid)
+        w=win32gui
+        window = w.GetForegroundWindow()
+        #print("window: %s" % window)
+        if (window != 0):
+            if (w.GetClassName(window) == 'CabinetWClass'): # the main explorer window
+                #print("class: %s" % w.GetClassName(window))
+                #print("text: %s " %w.GetWindowText(window))
+                children = list(set(self.searchChildWindows(window)))
+                addr_edit = None
+                file_view = None
+                for child in children:
+                    if (w.GetClassName(child) == 'WorkerW'): # the address bar
+                        addr_children = list(set(self.searchChildWindows(child)))
+                        for addr_child in addr_children:
+                            if (w.GetClassName(addr_child) == 'ReBarWindow32'):
+                                addr_edit = addr_child
+                                addr_children = list(set(self.searchChildWindows(child)))
+                                for addr_child in addr_children:
+                                    if (w.GetClassName(addr_child) == 'Address Band Root'):
+                                        addr_edit = addr_child
+                                        addr_children = list(set(self.searchChildWindows(child)))
+                                        for addr_child in addr_children:
+                                            if (w.GetClassName(addr_child) == 'msctls_progress32'):
+                                                addr_edit = addr_child
+                                                addr_children = list(set(self.searchChildWindows(child)))
+                                                for addr_child in addr_children:
+                                                    if (w.GetClassName(addr_child) == 'Breadcrumb Parent'):
+                                                        addr_edit = addr_child
+                                                        addr_children = list(set(self.searchChildWindows(child)))
+                                                        for addr_child in addr_children:
+                                                            if (w.GetClassName(addr_child) == 'ToolbarWindow32'):
+                                                                text=self.getEditText(addr_child)
+                                                                if "\\" in text:
+                                                                    self.address_1=self.getEditText(addr_child)[text.index(" ")+1:]
+                                                                    # print("Address --> "+self.address_1)
+
+        for window in range(shellwindows.Count):
+            window_url = urllib.parse.unquote(shellwindows[window].LocationURL,encoding='ISO 8859-1')
+            self.window_dir = window_url.split("///")[1].replace("/", "\\")
+            # print("Directory --> "+self.window_dir)
+            if self.window_dir==self.address_1:
+                selected_files = shellwindows[window].Document.SelectedItems()
+                for file in range(selected_files.Count):
+                    self.files.append(selected_files.Item(file).Path)
+                # print("Files --> "+str(self.files))
+        return self.window_dir, self.files
+    
+    def create_new_file(self, query):
+        # create a new file of python name index
+        directory, files = self.explorer_fileselection()
+        extension = ''.join(query.split()[5])
+        file_name = ''.join(query.split()[7:])
+        self.extension = extension
+        self.file_name = file_name
+
+        if "python" in self.extension:
+            exe = ".py"
+        elif "javascript" in self.extension:
+            exe = ".js"
+        elif "php" in self.extension:
+            exe = ".php"
+        elif "html" in self.extension:
+            exe = ".html"
+        elif "css" in self.extension:
+            exe = ".css"
+        else:
+            exe = ".txt"
+
+        full_file = f"{self.file_name}{exe}"
+        complete_name = os.path.join(directory, full_file)
+        with open(complete_name, "w"):
+            pass
+        
 # while True:
 #     try:
-#         explorer_fileselection()
+#         main = file_management()
+#         # direc, files = main.explorer_fileselection()
 #     except Exception:
 #         print("No Path Found!")
 #     time.sleep(1)
+
+main = file_management()
+time.sleep(5)
+main.create_new_file("create a new file of html name index")
