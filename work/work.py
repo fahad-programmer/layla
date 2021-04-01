25526fecca4121bd1a429e0422a97ee0e2e98322
imran-prog <infotainer04@gmail.com>
2021-04-02 00:21:07 +0500

    Solved Some Bugs

--- work/work.py
+++ work/work.py
@@ -42,6 +42,11 @@ import random, datetime
 import pyaudio
 import wave
 import win32gui, win32api
+import http.server
+import socket, socketserver
+import pyqrcode
+from pyqrcode import QRCode
+import png, os
 # Importing user made Modules
 from work import wishing, keyboard_controller, sound, locate_me, main_api, tictactoe, rps_game, sys_info
 from work.dice import dice_roller
@@ -1431,6 +1436,65 @@ def audio_recorder():
     # close the file
     wf.close()
 
+@lru_cache()
+def file_sharing():
+    '''
+    Returns the sum of two decimal numbers in binary digits.
+
+        Parameters:
+            a (int): A decimal integer
+            b (int): Another decimal integer
+
+        Returns:
+            binary_sum (str): Binary string of the sum of a and b
+    '''
+    # assinging the appropriate port value
+    PORT = 8010
+    # this finds the name of the computer user
+    os.environ['USERPROFILE']
+    
+    
+    # changing the directory to access the files desktop
+    # with the help of os module
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']),
+                        'OneDrive')
+    os.chdir(desktop)
+    
+    
+    # creating a http request
+    Handler = http.server.SimpleHTTPRequestHandler
+    # returns, host name of the system under
+    # which Python interpreter is executed
+    hostname = socket.gethostname()
+    
+    
+    # finding the IP address of the PC
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    IP = "http://" + s.getsockname()[0] + ":" + str(PORT)
+    link = IP
+    
+    
+    # converting the IP address into the form of a QRcode
+    # with the help of pyqrcode module
+    
+    # converts the IP address into a Qrcode
+    url = pyqrcode.create(link)
+    # saves the Qrcode inform of svg
+    url.svg("myqr.svg", scale=8)
+    # opens the Qrcode image in the web browser
+    webbrowser.open('myqr.svg')
+    
+    
+    # Creating the HTTP request and  serving the
+    # folder in the PORT 8010,and the pyqrcode is generated
+    
+    # continuous stream of data between client and server
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        print("Type this in your Browser", IP)
+        print("or Use the QRCode")
+        httpd.serve_forever()
 
 if __name__ == '__main__':
     """
